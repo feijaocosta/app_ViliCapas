0d1b3d809fd9ce3d86ef6165c6044625d2968a89
VFBC <44851096+VFBC@users.noreply.github.com>
2018-11-21 21:06:58 -0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -26,7 +26,7 @@ if (typeof files == 'undefined') {
   files.push('./');
 }
 
-var CACHE_NAME = 'vili.1';
+var CACHE_NAME = 'vili.V2';
 
 self.addEventListener('activate', function(event) {
   console.log('[SW] Activate');
