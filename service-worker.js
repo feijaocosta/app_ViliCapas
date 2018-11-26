a01c7093c84bf493115da260753ef84a0c92967b
Feijao Costa <feijao@feijaocosta.com.br>
2018-11-26 20:26:42 -0200

    Update service-worker.js
    teste

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,5 @@
 var files = [
-  "/app_ViliCapas/index.html",
-  "/app_ViliCapas/manifest.json",  
-  "/app_ViliCapas/css/style.css",
-  "/app_ViliCapas/imagens/bannerfull.png",
-  "/app_ViliCapas/imagens/bannertarja.png",
-  "/app_ViliCapas/imagens/botaocarrinho.png",
-  "/app_ViliCapas/imagens/botaomenu.png",
-  "/app_ViliCapas/imagens/botaopesquisa.png",
-  "/app_ViliCapas/imagens/logo.png",
-  "/app_ViliCapas/imagens/Produtopreto.png",
-  "/app_ViliCapas/imagens/Produtorosa.png",
-  "/app_ViliCapas/images/icons/icon-32x32.png",
-  "/app_ViliCapas/images/icons/icon-128x128.png",
-  "/app_ViliCapas/images/icons/icon-144x144.png",
-  "/app_ViliCapas/images/icons/icon-152x152.png",
-  "/app_ViliCapas/images/icons/icon-192x192.png",
-  "/app_ViliCapas/images/icons/icon-256x256.png",
-  "/app_ViliCapas/images/icons/icon-512x512.png"
-  
+  "/app_ViliCapas/index.html"
 ];
 // dev only
 if (typeof files == 'undefined') {
@@ -26,7 +8,7 @@ if (typeof files == 'undefined') {
   files.push('./');
 }
 
-var CACHE_NAME = 'vili.V4';
+var CACHE_NAME = 'vili.V5';
 
 self.addEventListener('activate', function(event) {
   console.log('[SW] Activate');
